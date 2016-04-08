4524a840f6dd4255db41e00099a5c0e2d2c7368e
sreepraneeth K <sreepraneeth K>
2016-04-08 22:12:41 +0530

    Side pannel implementation

---
+++ quiz/extras/Side_Panel/panel.js
@@ -0,0 +1,80 @@
+function init()
+{
+ var limit=Number(prompt("Enter number of buttons to be placed",""));
+ if(limit>10)
+	document.getElementById("scrollbar").style.height=40+"em";
+ for(var i=1;i<=limit;i++)
+	addButtonElement(i);
+}
+
+function addButtonElement(num)
+{
+  var ni = document.getElementById('panel');
+  var newdiv = document.createElement('button');
+  var divIdName = 'button'+num;
+  newdiv.setAttribute('id',divIdName);
+  newdiv.setAttribute("type", "button");
+  newdiv.setAttribute("class", "btn btn-default");
+  newdiv.onclick=quesDiv;
+  newdiv.onactivate=quesDiv;
+  newdiv.innerHTML = num+'<br/>';
+  ni.appendChild(newdiv);
+  
+  ni=document.getElementById(divIdName);
+  newdiv = document.createElement('a');
+  divIdName = 'button'+num+'anchor';
+  newdiv.setAttribute('id',divIdName);
+  newdiv.setAttribute('href','#');
+  newdiv.onclick=markUnmark;
+  newdiv.innerHTML = 'Click to mark';
+  ni.appendChild(newdiv);
+
+/*var ni = document.getElementById('panel');
+  var newdiv = document.createElement('div');
+  var divIdName = 'button'+num+'div';
+  newdiv.setAttribute('id',divIdName);
+  divIdName = 'button'+num;
+ newdiv.innerHTML = '<button  type=\"button\" class=\"btn btn-default\" id=\"'+divIdName+'\" >'+num+'<br/><a href=\"#\" onclick=\"markUnmark(\''+divIdName+'\')\">Click to mark</a></button>';
+  ni.appendChild(newdiv);
+
+  newdiv.innerHTML = num+'<br/><a href=\"#\" onclick=\"markUnmark(\''+divIdName+'\')\">Click to mark</a>';
+
+*/
+
+}
+
+function quesDiv()
+{
+  var ni = document.getElementById("ques");
+  var i= this.id.substring( this.id.search(/\d/) );
+  ni.innerHTML="Question Number "+i+"<br/>"; 
+  var newdiv = document.createElement('button');
+  var divIdName = 'nextQues'+( Number(i) +1 );
+  newdiv.setAttribute('id',divIdName);
+  newdiv.onclick=nextQues;
+  newdiv.innerHTML = 'Next Question'+ (Number(i) + 1 );
+  ni.appendChild(newdiv);
+}
+
+function nextQues()
+{
+  var i= this.id.substring( this.id.search(/\d/) );
+  document.getElementById('button'+i).click();
+}
+
+
+function markUnmark()
+{
+  var id=this.id.substring(0,this.id.length-6)
+  var b=document.getElementById(id);
+  if(b.className.search("btn btn-default") == 0 )
+		{
+		b.className="btn btn-warning";
+		this.innerHTML="Click to unmark";
+	}
+  else
+	{
+		b.className="btn btn-default";
+		this.innerHTML="Click to mark";
+	}
+}
